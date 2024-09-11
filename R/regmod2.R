a73e04d0352f56ab28884bbc285fba1c5d23b66b
wgcantah <william.cantah@ucc.edu.gh>
2024-09-11 12:53:13 +0000

    Initial commit

---
+++ R/regmod2.R
@@ -0,0 +1,24 @@
+extract_model_diagnostics <- function(x) {
+  # Adjusted R-squared
+  adj_r_squared <- summary(x)$adj.r.squared
+
+  # AIC and BIC
+  aic_value <- AIC(x)
+  bic_value <- BIC(x)
+
+  # F-statistics and p-value
+  f_stat <- summary(x)$fstatistic
+  f_value <- f_stat[1]
+  f_p_value <- pf(f_value, f_stat[2], f_stat[3], lower.tail = FALSE)
+
+  # Create a data frame with the results
+  diagnostics_df <- data.frame(
+    R.Bar = adj_r_squared,
+    AIC = aic_value,
+    BIC = bic_value,
+    F.Statistic = f_value,
+    F.Pvalue = f_p_value
+  )
+
+  return(diagnostics_df)
+}
